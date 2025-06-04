1e74981a3f4f42fc7c2d826630fd7dbbd488f1b6
duridev <duridev@outlook.com>
2025-06-03 23:49:17 -0400

    resolucioón del ejercicio propuesto c#

--- ejercicio2-modulo1/ejercicio3.cs
+++ ejercicio2-modulo1/ejercicio3.cs
@@ -6,7 +6,18 @@ namespace ejercicio2_modulo1
     {
         static void Main(string[] args)
         {
+            float kilometrosEntre2Ciudades, velocidadVehiculo, tiempoDeConduccion;
 
+
+            Console.WriteLine("Ingresar la distancia entre dos ciudades");
+            kilometrosEntre2Ciudades = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresar velocidad del vehículo");
+            velocidadVehiculo = float.Parse(Console.ReadLine());
+
+            tiempoDeConduccion = kilometrosEntre2Ciudades / velocidadVehiculo;
+
+            Console.WriteLine("El tiempo de conducción es de: " + tiempoDeConduccion.ToString("0.00") + " horas");
         }
     }
 }
